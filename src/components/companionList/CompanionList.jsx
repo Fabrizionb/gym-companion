4167c60c1b81f0c222ec3144b5629120ffde8509
Fabrizio <100951814+Fabrizionb@users.noreply.github.com>
2023-03-13 23:03:11 +0100

    filter

--- src/components/companionList/CompanionList.jsx
+++ src/components/companionList/CompanionList.jsx
@@ -5,17 +5,12 @@ import BotonCalcular from "../boton/Boton";
 import "./CompanionList.css";
 
 const CompanionList = ({ data, sortMinus, sortPlus }) => {
-  const [filter, setfilter] = useState("");
-  const [bodyPart, setbodyPart] = useState("cuello");
-  const [muscle, setMuscle] = useState("");
+  const [bodyPart, setBodyPart] = useState("");
+  const [target, setTarget] = useState("");
   const [equipment, setEquipment] = useState("");
 
-  const [selectedBodyPart, setSelectedBodyPart] = useState("");
-  const [selectedTarget, setSelectedTarget] = useState("");
-  const [selectedEquipment, setSelectedEquipment] = useState("");
-
-  const bodyPartOptions = [...new Set(data.map(item => item.bodyPart))];
-
+  const [filter, setfilter] = useState("");
+  const [muscle, setMuscle] = useState("");
 
   const filteredData = data.filter(function (element) {
     return (
@@ -30,7 +25,6 @@ const CompanionList = ({ data, sortMinus, sortPlus }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(6);
   const [lastPage, setLastPage] = useState(0);
-
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -48,14 +42,38 @@ const CompanionList = ({ data, sortMinus, sortPlus }) => {
 
   const handleClick = (event) => {
     event.preventDefault(); // Evita que la página se recargue al hacer submit
-    setbodyPart("");
+
     setMuscle("");
     setEquipment("");
     setCurrentPage(1);
     setLastPage(1);
+
+    setEquipment("")
+    setBodyPart("")
+    setTarget("")
   };
 
   useEffect(() => {}, [bodyPart, muscle, equipment]);
+  // Obtener lista de opciones únicas para el primer select
+  const bodyPartOptions = Array.from(
+    new Set(data.map((item) => item.bodyPart))
+  );
+  // Obtener lista de opciones únicas para el segundo select
+  const targetOptions = Array.from(
+    new Set(
+      data
+        .filter((item) => item.bodyPart === bodyPart)
+        .map((item) => item.target)
+    )
+  );
+  // Obtener lista de opciones únicas para el tercer select
+  const equipmentOptions = Array.from(
+    new Set(
+      data
+        .filter((item) => item.bodyPart === bodyPart && item.target === target)
+        .map((item) => item.equipment)
+    )
+  );
 
   return (
     <div className='container companion mt-2 pt-3'>
@@ -72,136 +90,57 @@ const CompanionList = ({ data, sortMinus, sortPlus }) => {
         explorar nuestra base de datos de ejercicios y esperamos que le resulte
         útil para alcanzar sus metas de fitness.
       </div>
-      <form className='row'>
-        <select
-          className='browser-default custom-select mb-2 col-4'
-          value={bodyPart}
-          onChange={(event) => setbodyPart(event.target.value)}>
-          <option value='' disabled>
-            Seleccione una sección del cuerpo
-          </option>
-          <option value='espalda'>espalda</option>
-          <option value='cardio'>cardio</option>
-          <option value='pecho'>pecho</option>
-          <option value='antebrazos'>antebrazos</option>
-          <option value='piernas inferiores'>piernas inferiores</option>
-          <option value='cuello'>cuello</option>
-          <option value='hombros'>hombros</option>
-          <option value='brazos superiores'>brazos superiores</option>
-          <option value='piernas superiores'>piernas superiores</option>
-          <option value='cintura'>cintura</option>
-        </select>
 
-        <select
-          className='browser-default custom-select mb-2 col-4'
-          value={muscle}
-          onChange={(event) => setMuscle(event.target.value)}>
-          <option value='' disabled>
-            Seleccione un músculo del cuerpo
-          </option>
-          <option value='abductores'>abductores</option>
-          <option value='abdominales'>abdominales</option>
-          <option value='aductores'>aductores</option>
-          <option value='bíceps'>bíceps</option>
-          <option value='pantorrillas'>pantorrillas</option>
-          <option value='sistema cardiovascular'>sistema cardiovascular</option>
-          <option value='deltoides'>deltoides</option>
-          <option value='antebrazos'>antebrazos</option>
-          <option value='glúteos'>glúteos</option>
-          <option value='isquiotibiales'>isquiotibiales</option>
-          <option value='lumbares'>lumbares</option>
-          <option value='elevador de la escapula'>
-            elevador de la escapula
-          </option>
-          <option value='pectoral'>pectoral</option>
-          <option value='cuadriceps'>cuadriceps</option>
-          <option value='serrato anterior'>serrato anterior</option>
-          <option value='columna vertebral'>columna vertebral</option>
-          <option value='trapecios'>trapecios</option>
-          <option value='tríceps'>tríceps</option>
-          <option value='espalda alta'>espalda alta</option>
-        </select>
+      <div className='container pt-4 w-100'>
+        <div className='col'>
 
-        <select
-          className='browser-default custom-select mb-2 col-4'
-          value={equipment}
-          onChange={(event) => setEquipment(event.target.value)}>
-          <option value='' disabled>
-            Seleccione un equipamiento
-          </option>
-          <option value='asistido'>asistido</option>
-          <option value='banda elastica'>banda elastica</option>
-          <option value='barra con pesas'>barra con pesas</option>
-          <option value='peso corporal'>peso corporal</option>
-          <option value='pelota Bosu'>pelota Bosu</option>
-          <option value='maquina de cable'>maquina de cable</option>
-          <option value='pesa (mancuerna)'>pesa (mancuerna)</option>
-          <option value='maquina elíptica'>maquina elíptica</option>
-          <option value='barra EZ'>barra EZ</option>
-          <option value='martillo'>martillo</option>
-          <option value='pesa rusa (kettlebell)'>pesa rusa (kettlebell)</option>
-          <option value='maquina de palanca'>maquina de palanca</option>
-          <option value='balón medicinal'>balón medicinal</option>
-          <option value='barra olímpica'>barra olímpica</option>
-          <option value='banda de resistencia'>banda de resistencia</option>
-          <option value='rodillo de espuma'>rodillo de espuma</option>
-          <option value='cuerda'>cuerda</option>
-          <option value='maquina de esquí (skierg)'>
-            maquina de esquí (skierg)
-          </option>
-          <option value='maquina de arrastre'>maquina de arrastre</option>
-          <option value='maquina Smith'>maquina Smith</option>
-          <option value='pelota de estabilidad'>pelota de estabilidad</option>
-          <option value='bicicleta estática'>bicicleta estática</option>
-          <option value='escaladora de escalones'>
-            escaladora de escalones
-          </option>
-          <option value='neumático'>neumático</option>
-          <option value='barra trampa'>barra trampa</option>
-          <option value='ergómetro de cuerpo superior'>
-            ergómetro de cuerpo superior
-          </option>
-          <option value='con peso'>con peso</option>
-          <option value='rodillo de rueda'>rodillo de rueda</option>
-        </select>
-        <BotonCalcular title={"Borrar Filtros"} handleClick={handleClick} />
-      </form>
-      <div className='row pt-4 w-100'>
-        {/* <!-- Sidebar --> */}
-        <select value={selectedBodyPart} onChange={(e) => setSelectedBodyPart(e.target.value)}>
-  <option value="">Selecciona Body Part</option>
-  {bodyPartOptions.map((option) => (
-    <option key={option} value={option}>
-      {option}
-    </option>
-  ))}
-</select>
 
-<select value={selectedTarget} onChange={(e) => setSelectedTarget(e.target.value)}>
-  <option value="">Selecciona Target</option>
-  {data
-    .filter((item) => item.bodyPart === selectedBodyPart)
-    .map((item) => item.target)
-    .filter((value, index, self) => self.indexOf(value) === index)
-    .map((option) => (
-      <option key={option} value={option}>
-        {option}
-      </option>
-    ))}
-</select>
 
-<select value={selectedEquipment} onChange={(e) => setSelectedEquipment(e.target.value)}>
-  <option value="">Selecciona Equipamiento</option>
-  {data
-    .filter((item) => item.bodyPart === selectedBodyPart && item.target === selectedTarget)
-    .map((item) => item.equipment)
-    .filter((value, index, self) => self.indexOf(value) === index)
-    .map((option) => (
-      <option key={option} value={option}>
-        {option}
-      </option>
-    ))}
-</select>
+          {/* <!-- Sidebar --> */}
+          <div className='row d-flex '>
+            <select
+              id='bodyPartSelect'
+              value={bodyPart}
+              onChange={(e) => setBodyPart(e.target.value)} className='selects col-4 form-select'>
+              <option value=''>Selecciona una parte del cuerpo</option>
+              {bodyPartOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <select
+              id='targetSelect'
+              value={target}
+              onChange={(e) => setTarget(e.target.value)} className='selects col-4 form-select'>
+              <option value=''>Selecciona un músculo</option>
+              {targetOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <select
+              id='equipmentSelect'
+              value={equipment}
+              onChange={(e) => setEquipment(e.target.value)} className='selects col-4 form-select'  >
+              <option value=''>Selecciona un equipamiento </option>
+              {equipmentOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <div className='col-12 mt-3'>
+              <BotonCalcular
+                title={"Borrar Filtros"}
+                handleClick={handleClick}
+              />
+            </div>
+          </div>
+        </div>
         {/* <!-- Sidebar --> */}
 
         {/* <!-- Content --> */}
@@ -225,23 +164,26 @@ const CompanionList = ({ data, sortMinus, sortPlus }) => {
                   />
                 ))
               )}
-              <divc className='paginationContainer d-flex align-items-center justify-content-center'>
+              <div className='paginationContainer d-flex align-items-center justify-content-center'>
                 <button
                   type='button'
-                  class='btn btn-secondary btn-sm'
-                  onClick={prevPage} disabled={currentPage === 0}>
+                  className='btn btn-secondary btn-sm'
+                  onClick={prevPage}
+                  disabled={currentPage === 0}>
                   Anterior
                 </button>
                 <span className='paginationPage btn'>
-                  Page: {currentPage} de { Math.round(filteredData.length / itemsPerPage)}
+                  Page: {currentPage} de{" "}
+                  {Math.round(filteredData.length / itemsPerPage)}
                 </span>
                 <button
                   type='button'
-                  class='btn btn-secondary btn-sm'
-                  onClick={nextPage} disabled={currentPage === filteredData.length}>
+                  className='btn btn-secondary btn-sm'
+                  onClick={nextPage}
+                  disabled={currentPage === filteredData.length}>
                   Siguiente
                 </button>
-              </divc>
+              </div>
             </div>
             {/* <!-- Grid row --> */}
           </section>
